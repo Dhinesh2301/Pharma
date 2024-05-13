16cd3e3c7a614c0c03a768ea9375762012ac6183
Dhinesh2301 <rm3317428@gmail.com>
2024-05-13 19:41:45 +0530

    success

--- KAMALESH/KAMALESH/urls.py
+++ KAMALESH/KAMALESH/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for TVK project.
+URL configuration for KAMALESH project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
--- KAMALESH/manage.py
+++ KAMALESH/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TVK.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'KAMALESH.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
